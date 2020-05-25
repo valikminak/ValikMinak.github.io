9fcf0d1582102340505fcc3ada55e00f049d9dd5
ValikMinak <58771677+ValikMinak@users.noreply.github.com>
2020-05-25 13:00:17 +0300

    Books

---
+++ Books/precache-manifest.614c7bcdfe7497114edc2d4218e78848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ff2aca492b8b80034ebcdb6a5d392a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4220580fd2025a50ee9f",
+    "url": "/static/css/main.da380692.chunk.css"
+  },
+  {
+    "revision": "c384e0d4751faad4586c",
+    "url": "/static/js/2.1346ee78.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.1346ee78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4220580fd2025a50ee9f",
+    "url": "/static/js/main.a6e0e76f.chunk.js"
+  },
+  {
+    "revision": "b9dc0ec55d81e2efd17d",
+    "url": "/static/js/runtime-main.252b7c44.js"
+  }
+]);
